6a92f588af68e6df385aa001283503e5ff8710d8
ClientSysFWGit <clientsysfwgit@intel.com>
2025-04-04 15:42:46 +0530

    [PTL] TxPiOn, InternalClocksOn, IntCkOn must stay Enabled
    [Feature Description]
    TxPiOn, InternalClocksOn, and IntCkOn needs to remain 1 which
    is set part of Phy Init.
    MRC should not be overriding these register fields in DdrIoFinalize
    
    Additional change: remove the maximum frequency changes
    done recently for the LP5 1R config.
    
    Package/Module: MemoryInit
    
    [Impacted Platform]
    All
    
    Hsd-es-id: 18041834983
    Original commit date: Wed Mar 19 12:06:54 2025 +0100
    Change-Id: Iae2be064e0c08ab3958e42e6d3bdace6f0eca17f
    Original commit hash: 6f6fcdb7df58dfa00eafa2fdf6b01cd5a40c9d50

--- OneSiliconPkg/IpBlock/MemoryInit/src/PmaLib/Ptl/BluePmMessageApiPtl.c
+++ OneSiliconPkg/IpBlock/MemoryInit/src/PmaLib/Ptl/BluePmMessageApiPtl.c
@@ -225,12 +225,7 @@ MrcMcCapabilityPreSpd (
       return Status;
     }
     if (Outputs->IsLpddr5) {
-      if (Outputs->IsAny2Ranks) {
-        Outputs->FreqMax = MIN (Outputs->FreqMax, f7467); // Default SAGV: 2400, 4800, 6400, 7467 (all G4)
-      } else {
-        Outputs->FreqMax = MIN (Outputs->FreqMax, f8533); // Default SAGV: 2400, 4800, 6400, 8533 (all G4)
-      }
-
+      Outputs->FreqMax = MIN (Outputs->FreqMax, f7467); // Default SAGV: 2400, 4800, 6400, 7467 (all G4)
     } else { // DDR5
       Outputs->FreqMax = MIN (Outputs->FreqMax, f6400); // Default SAGV: 3200, 4800, 5600, 6400 (all G4)
     }
