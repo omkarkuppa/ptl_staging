022d4349d8d92fc0e89ec09ee7f48587aa737d75
ClientSysFWGit <clientsysfwgit@intel.com>
2026-04-09 11:49:40 +0530

    Revert "Add version dependency for Media Aggressive throttling config"
    Tue Mar 31 12:00:51 2026 +0530
    Original commit date: Tue Mar 31 12:00:51 2026 +0530
    Original commit hash: d416b47b061d6118ecda80c98ed048f0759455f5

--- OneSiliconPkg/IpBlock/CpuPowerManagement/LibraryPrivate/PeiCpuPowerManagementLib/PowerMgmtPreResetCpl.c
+++ OneSiliconPkg/IpBlock/CpuPowerManagement/LibraryPrivate/PeiCpuPowerManagementLib/PowerMgmtPreResetCpl.c
@@ -30,8 +30,6 @@
 #include <Register/Cpuid.h>
 #include <Library/CpuPlatformLib.h>
 #include <Library/CpuInfoFruLib.h>
-#include <Library/PeiMeLib.h>
-#include <Library/BaseMemoryLib.h>
 #include <Register/B2pMailbox.h>
 
 /**
@@ -360,9 +358,6 @@ ConfigPowerFloorMgmt (
   PCODE_MAILBOX_INTERFACE          MailboxCommand;
   MAILBOX_DATA_POWER_FLOOR_CONFIG  MailboxData;
   BOOLEAN                          SettingsChanged;
-  FLASH_PARTITION_DATA             PcodePartitionData;
-  UINT32                           PcodeNumOfModules;
-  BOOLEAN                          PcodeVersionValid;
 
   SettingsChanged  = FALSE;
   MailboxData.Data = 0;
@@ -424,44 +419,6 @@ ConfigPowerFloorMgmt (
     MailboxData.Fields.PowerFloorPcieGenXDowngradeDisable = !CpuPowerMgmtBasicConfig->PowerFloorPcieGenDowngrade;
     SettingsChanged = TRUE;
   }
-
-  //
-  // Check pcode version before modifying Media Aggressive throttling option
-  //
-  PcodeVersionValid = FALSE;
-  PcodeNumOfModules = 0;
-  ZeroMem (&PcodePartitionData, sizeof (FLASH_PARTITION_DATA));
-
-  Status = PeiHeciGetImageFwVersionMsg (FPT_PARTITION_NAME_PCOD, &PcodeNumOfModules, &PcodePartitionData);
-  if (!EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_INFO, "ConfigPowerFloorMgmt: Pcode version: %d.%d.%d.%d\n",
-      PcodePartitionData.Version.Major, PcodePartitionData.Version.Minor,
-      PcodePartitionData.Version.Hotfix, PcodePartitionData.Version.Build));
-    //
-    // Only enable Media Aggressive throttling config for proper pcode version
-    //
-    if ((PcodePartitionData.Version.Major > 9) ||
-        ((PcodePartitionData.Version.Major == 9) && (PcodePartitionData.Version.Minor > 0)) ||
-        ((PcodePartitionData.Version.Major == 9) && (PcodePartitionData.Version.Minor == 0) && (PcodePartitionData.Version.Hotfix >= 33))) {
-      PcodeVersionValid = TRUE;
-    }
-  }
-  //
-  // Check Media Aggressive throttling option (reverse encoding is used)
-  //
-  if (PcodeVersionValid &&
-      (MailboxData.Fields.PowerFloorAggressiveMediaDisable == CpuPowerMgmtBasicConfig->PowerFloorAggressiveMedia)) {
-    //
-    // When enabled, SoC uses aggressive media throttling to lower SoC floor power.
-    // Mailbox parameter is to disable the option and thus have reverse encoding, 0: Enabled(Default), 1: Disabled
-    // Hence requested value of 1 for the mailbox data means aggressive media throttling will be disabled.
-    // Once it is disabled, it remains disabled till next warm/cold reset.
-    //
-    DEBUG ((DEBUG_INFO, "ConfigPowerFloorMgmt: User wants to modify Media Aggressive throttling option for SOC floor power management \n"));
-    MailboxData.Fields.PowerFloorAggressiveMediaDisable = !CpuPowerMgmtBasicConfig->PowerFloorAggressiveMedia;
-    SettingsChanged = TRUE;
-  }
-
   if (SettingsChanged) {
     DEBUG ((DEBUG_INFO, "ConfigPowerFloorMgmt: Writing value 0x%x to the mailbox\n", MailboxData.Data));
     MailboxCommand.InterfaceData = 0;
